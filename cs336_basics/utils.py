f58bedae48fd4f8f7e4acc15f11a918dea0b725d
orion-t8 <tangziye2025@outlook.com>
2026-09-06 20:12:13 +0800

    Finish cross-entropy. Learnt torch.gather as well.

--- cs336_basics/utils.py
+++ cs336_basics/utils.py
@@ -1,6 +1,7 @@
 import torch
 import einops
 import numpy as np
+from jaxtyping import Float, Int
 
 def softmax(x: torch.Tensor, i: int) -> torch.Tensor:
     m = torch.max(x, dim=i, keepdim=True).values
@@ -13,3 +14,9 @@ def scaled_dot_product_attn(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, m
         z[~mask] = -torch.inf
     factor = softmax(z, i=-1)
     return einops.einsum(factor, V, "batch_size ... seq_len_q seq_len_k, batch_size ... seq_len_k d_v -> batch_size ... seq_len_q d_v")
+
+def cross_entropy(logits: Float[torch.Tensor, "batch vocab_size"], targets: Int[torch.Tensor, "batch"]) -> Float[torch.Tensor, ""]:
+    subtracted = logits - torch.max(logits, dim=-1, keepdim=True).values # shape = (batch, )
+    logsumexp = torch.log(torch.sum(torch.exp(subtracted), dim=-1))
+    selected = torch.gather(subtracted, dim=-1, index=targets.unsqueeze(-1)).squeeze(-1)
+    return torch.mean(logsumexp - selected)
--- tests/adapters.py
+++ tests/adapters.py
@@ -21,7 +21,11 @@ from cs336_basics.model import (
     TransformerBlock,
     TransformerLM
 )
-from cs336_basics.utils import softmax, scaled_dot_product_attn
+from cs336_basics.utils import (
+    softmax,
+    scaled_dot_product_attn,
+    cross_entropy
+)
 
 
 def run_linear(
@@ -485,7 +489,7 @@ def run_cross_entropy(
     Returns:
         Float[Tensor, ""]: The average cross-entropy loss across examples.
     """
-    raise NotImplementedError
+    return cross_entropy(inputs, targets)
 
 
 def run_gradient_clipping(parameters: Iterable[torch.nn.Parameter], max_l2_norm: float) -> None:
